a9f2e6e28454798d20786b20ced10fd44195fd8b
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-07-21 22:25:54 +0200

    edit max width

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,17 +1,12 @@
-
-
-import Categories from "@/components/categories";
-import YesMove from "@/components/yes-move";
-import LandingBanner from "@/components/landing-banner";
-import Offer from "@/components/offer";
-import PopulaActivities from "@/components/popula-activities";
+import Categories from '@/components/categories';
+import YesMove from '@/components/yes-move';
+import LandingBanner from '@/components/landing-banner';
+import Offer from '@/components/offer';
+import PopulaActivities from '@/components/popula-activities';
 
 const LandingPage = () => {
-
-
   return (
-    <div className="h-full max-w-[1900px] max-w-sc mx-auto">
-
+    <div className="h-full max-w-[1600px] max-w-sc mx-auto">
       {/* landing banner */}
       <LandingBanner />
 
@@ -21,8 +16,12 @@ const LandingPage = () => {
       {/* available clubs */}
       <section>
         <div className="my-16 px-3 lg:px-16 mx-auto">
-          <h2 className=" text-black dark:text-white font-bold text-2xl 2xl:text-3xl text-center">Classes available in our clubs</h2>
-          <p className=" text-[12px] 2xl:text-lg text-center text-gray-500  mt-4">Choose your favorite activities and learn more</p>
+          <h2 className=" text-black dark:text-white font-bold text-2xl 2xl:text-3xl text-center">
+            Classes available in our clubs
+          </h2>
+          <p className=" text-[12px] 2xl:text-lg text-center text-gray-500  mt-4">
+            Choose your favorite activities and learn more
+          </p>
 
           <Categories />
         </div>
@@ -35,6 +34,6 @@ const LandingPage = () => {
       <YesMove />
     </div>
   );
-}
+};
 
 export default LandingPage;
