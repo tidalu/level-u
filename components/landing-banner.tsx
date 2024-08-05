80781af3ba45a6163c555b72ee859e3512cde2ac
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-05 04:24:45 +0200

    modified landing banner

--- components/landing-banner.tsx
+++ components/landing-banner.tsx
@@ -4,39 +4,48 @@ import React from 'react';
 import HomeBlogList from './home-blogList';
 import { Button } from './ui/button';
 import { data } from '@/data';
+import Forms from './Forms';
 
 const LandingBanner = () => {
   return (
-    <div className="  homeBanner pt-32 pb-12 rounded-b-3xl px-3 lg:px-16 relative overflow-hidden 3xl:mx-32 ">
+    <div className="  homeBanner pt-32 pb-12 rounded-b-3xl px-5 lg:px-16 relative overflow-hidden 3xl:mx-32  ">
       <div className=" relative rounded-2xl overflow-hidden  md:h-[450px] grid grid-cols-12 bg-[#6cce40] mx-auto">
         <div className=" text-white p-6 lg:py-12 lg:pl-16 col-span-12 md:col-span-5">
-          <p className=" text-[12px] font-semibold">
-            {data.landingAdd.topText}
-          </p>
-          <h1 className=" text-2xl mt-1 mb-4 font-extrabold">
-            {data.landingAdd.header}
-          </h1>
-          <p className=" text-[12px] text-gray-200">
-            {data.landingAdd.description}
-          </p>
-          <ul className="text-[12px] text-gray-200 my-3">
-            {data.landingAdd.bulletPoints.map((b, i) => (
-              <li key={i} className="mb-1.5">
-                {' '}
-                - {b}
-              </li>
-            ))}
-          </ul>
+          <div className="w-full ">
+            <p className="text-4xl text-left text-black xl:text-5xl 2xl:text-6xl font-extrabold leading-snug italic dark:text-">
+              Pre-Registration
+            </p>
+            <p className="text-4xl text-right lg:-mr-8 xl:text-5xl 2xl:text-6xl xl:-mr-11 text-black font-extrabold leading-snug italic ">
+              is open now
+            </p>
+          </div>
 
-          <div className="flex flex-col md:flex-row gap-4 md:gap-x-4 mt-4">
-            <Link href="/contact">
-              <Button
-                variant="secondary"
-                className="radial_bg_red shadow-xl text-white px-10 font-semibold  rounded-full text-[11px] sm:text-sm"
-              >
-                I{`'`}am buying
-              </Button>
-            </Link>
+          <div className="w-[200px] h-[240px] max-h-[60%] mx-auto 2xl:mx-0 2xl:self-right bg-[#FFD032] m-7 ">
+            {' '}
+            <div className="border-[3px] relative border-[#3F3F3F] w-full h-full -inset-3  text-center">
+              <div className="absolute  flex flex-row -top-[20px] left-[20px]">
+                <img
+                  src="/quote.png"
+                  width={70}
+                  height={70}
+                  alt="quote"
+                  className="stroke-cyan-700"
+                />
+              </div>
+              <div className="absolute  flex flex-row -bottom-[25px] right-[20px] rotate-180">
+                <img
+                  src="/quote.png"
+                  width={70}
+                  height={70}
+                  alt="quote"
+                  className="stroke-cyan-700"
+                />
+              </div>
+              <p className="text-[#101210] text-wrap text-center p-7 pl-10 font-bold  tracking-wide h-full flex justify-center items-center">
+                SIGN UP IN TIME TO THE GROUP AND GET THE OPPORTUNITY TO STUDY
+                ABROAD
+              </p>
+            </div>
           </div>
         </div>
 
