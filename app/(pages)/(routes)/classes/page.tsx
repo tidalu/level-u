080ff59b4124f06fad3d7e53000b617a5a5b96fa
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-09 16:30:48 +0200

    remove button, change bulletpoints to get dynamic from data

--- app/(pages)/(routes)/classes/page.tsx
+++ app/(pages)/(routes)/classes/page.tsx
@@ -9,7 +9,6 @@ import React, { useEffect, useRef, useState } from 'react';
 const ClassesPage = () => {
   const [isMuted, setIsMuted] = useState(true);
   const videoRef = useRef<HTMLVideoElement>(null);
-  
 
   const toggleMute = () => {
     setIsMuted((prev) => !prev);
@@ -50,35 +49,30 @@ const ClassesPage = () => {
             </Button>
           </div>
 
-          <div className=" text-gray-600 dark:text-gray-400 py-4 px-10 col-span-12 lg:col-span-5 xl:col-span-4">
-            <h1 className=" text-xl sm:text-2xl reveal mt-1 mb-4 text-[#6cce40] font-extrabold">
-              Find out how to exercise <br /> well in a group!
+          <div className=" text-gray-600 dark:text-gray-400 py-4 px-10 col-span-12 lg:col-span-5 xl:col-span-4 h-auto overflow-y-auto scrollbar-hide">
+            <h1 className=" text-xl sm:text-2xl reveal text-wrap mt-1 mb-4 text-[#6cce40] font-extrabold">
+              {data.classPage.mainContent.title}
             </h1>
             <p className=" text-[13px] reveal xl:text-[14px] 2xl:text-[15px] ">
-              In our clubs you benefit from the richest offer of group fitness
-              classes in Poland.
+              {data.classPage.mainContent.description[0]}
             </p>
             <ul className="text-[13px] reveal xl:text-[14px] 2xl:text-[15px]  my-3">
-              <li className="mb-1.5">Choose from workouts: </li>
-              <li className="mb-1.5"> - strengthening the body and fitness,</li>
-              <li className="mb-1.5"> - health-promoting exercises,</li>
-              <li className="mb-1.5"> - accelerating fat burning, </li>
-              <li className="mb-1.5"> - dancing, relaxation.</li>
+              {Array.isArray(data.classPage.mainContent.description[2]) &&
+                data.classPage.mainContent.description[2].map(
+                  (item: string, index: number) => (
+                    <li className="mb-1.5" key={index}>
+                      {' '}
+                      - {item} ,
+                    </li>
+                  )
+                )}
             </ul>
 
             <p className=" text-[13px] reveal xl:text-[14px] 2xl:text-[15px] ">
-              The offer also includes world-famous Les Mills training and our
-              own fitness classes, such as Anti-Stress or the extremely popular
-              stationary bike training.
+              {data.classPage.mainContent.description[3]}
             </p>
 
             <div className="flex reveal flex-col md:flex-row gap-4 md:gap-x-4 mt-4  mb-5 lg:mb-0">
-              <Button
-                variant="secondary"
-                className="radial_bg_red shadow-xl text-white px-10 font-semibold  rounded-full text-[11px] sm:text-sm"
-              >
-                Buy a pass
-              </Button>
               <Link
                 href="#category"
                 className="border-2 border-[#6cce40] cursor-pointer text-[11px] sm:text-sm rounded-full py-2 justify-center px-10 font-semibold flex gap-1 items-center hover:underline whitespace-nowrap"
