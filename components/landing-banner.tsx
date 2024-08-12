38c11a23f3244b40e74225639f62c215facc2468
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-12 06:02:55 +0200

    add button to trigger form

--- components/landing-banner.tsx
+++ components/landing-banner.tsx
@@ -6,12 +6,13 @@ import { Button } from './ui/button';
 import { data } from '@/data';
 import Forms from './Forms';
 import { TextGenerateEffect } from './ui/text-generate-effect';
+import { Form } from './form';
 
 const LandingBanner = () => {
   return (
     <div className=" reveal homeBanner pt-32 pb-12 rounded-b-3xl px-5 lg:px-16 relative overflow-hidden 3xl:mx-32  ">
       <div className=" relative rounded-2xl overflow-hidden   md:min-h-[450px] grid grid-cols-12 bg-[#6cce40] mx-auto">
-        <div className=" text-white p-6 lg:py-12 lg:pl-16 col-span-12 md:col-span-5 ">
+        <div className=" text-white p-6 lg:py-12 lg:pl-16 col-span-12 md:col-span-5 flex flex-col items-center ">
           <div className="w-full ">
             <p className="text-4xl reveal text-left text-black xl:text-5xl 2xl:text-6xl font-extrabold leading-snug italic apply-font-o ">
               Pre-Registration
@@ -21,7 +22,7 @@ const LandingBanner = () => {
             </p>
           </div>
 
-          <div className="w-[200px] h-[240px] max-h-[60%] mx-auto 2xl:mx-0 2xl:self-right bg-[#FFD032] m-7 ">
+          <div className="w-[80%] h-[240px] max-h-[60%] mx-auto 2xl:mx-0 2xl:self-right bg-[#FFD032] m-7 ">
             {' '}
             <div className="border-[3px] reveal relative border-[#3F3F3F] w-full h-full -inset-3  text-center">
               <div className="absolute   flex flex-row -top-[20px] left-[20px]">
@@ -48,7 +49,11 @@ const LandingBanner = () => {
               </p>
             </div>
           </div>
-          <div className="h-[30px]"></div>
+          <Form>
+            <div className="self-end text-gray-700 scale-110 hover:scale-105 transition-all delay-75 shadow-lg inline-flex items-center justify-center whitespace-nowrap  text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 radial-bg-green h-10 px-4 py-2 rounded-xl cursor-pointer">
+              Fill the form
+            </div>
+          </Form>
         </div>
 
         <div className="reveal bg-[url('/desk_home-banner.webp')] md:h-[100%] scale-y-110   bg-no-repeat bg-cover relative -right-6   -bottom-2 hidden md:block md:col-span-7 -mb-8"></div>
