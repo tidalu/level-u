d22fd410556b62e7e62549264ba1e302c0d4a107
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-16 16:17:33 +0200

    chore: Optimize landing page layout and add lazy loading for images

--- app/(pages)/(routes)/about/page.tsx
+++ app/(pages)/(routes)/about/page.tsx
@@ -1,14 +1,8 @@
 'use client';
-import dynamic from 'next/dynamic';
-import Image from 'next/image';
-import ExpandableText from '@/components/ExpandableText';
 import { useLocalizedData } from '@/lib/useLocalizedData';
 import React, { useEffect, useState } from 'react';
-import { NextSeo } from 'next-seo';
 
-import logo from '/public/logo.svg';
 import { InfiniteMovingCards } from '@/components/InfiniteMovingCards';
-import Particles from '@/components/magicui/particles';
 import { useTheme } from 'next-themes';
 import ScrollAnimateWrapper from '@/components/ScrollAnimateWrapper';
 import ThreeDTextScene from '@/components/ThreeDTextScene';
@@ -28,72 +22,65 @@ const AboutPage = () => {
   }, [reasons]);
 
   return (
-    <>
-      <NextSeo
-        title="Level Learning Center | About us"
-        description="The place where anything is possible to learn"
-        
-      />
-      <div className="h-full max-w-[1900px] mx-auto">
-        <ScrollAnimateWrapper>
-          <div className="min-h-[800px] flex flex-col items-center  justify-center pt-32 pb-12 rounded-b-3xl px-3 lg:px-16 border-red-700">
-            <div className=" relative rounded-2xl overflow-hidden  lg:h-[450px] grid grid-cols-12 bg-white  dark:bg-[#020817]  my-4 lg:my-10">
-              <div className=" relative col-span-12 reveal lg:col-span-7 xl:col-span-8 h-[300px] lg:h-auto flex xl:justify-end justify-center items-center overflow-hidden bg-slate-200">
-                <ThreeDTextScene />
-                <span className="  absolute bottom-4 left-0 flex w-full justify-center items-center">
-                  <p className=" text-center before:contents  mx-auto py-2 px-4 relative rounded-xl z-10 text-wrap w-auto text-with-blur">
-                    motto
-                  </p>
-                </span>
-              </div>{' '}
-              <div
-                className=" text-gray-600 dark:text-gray-400  py-4 px-10 col-span-12 lg:col-span-5 xl:col-span-4 overflow-y-scroll self-start"
+    <div className="h-full max-w-[1900px] mx-auto">
+      <ScrollAnimateWrapper>
+        <div className="min-h-[800px] flex flex-col items-center  justify-center pt-32 pb-12 rounded-b-3xl px-3 lg:px-16 border-red-700">
+          <div className=" relative rounded-2xl overflow-hidden  lg:h-[450px] grid grid-cols-12 bg-white  dark:bg-[#020817]  my-4 lg:my-10">
+            <div className=" relative col-span-12 reveal lg:col-span-7 xl:col-span-8 h-[300px] lg:h-auto flex xl:justify-end justify-center items-center overflow-hidden bg-slate-200">
+              <ThreeDTextScene />
+              <span className="  absolute bottom-4 left-0 flex w-full justify-center items-center">
+                <p className=" text-center before:contents  mx-auto py-2 px-4 relative rounded-xl z-10 text-wrap w-auto text-with-blur">
+                  motto
+                </p>
+              </span>
+            </div>{' '}
+            <div
+              className=" text-gray-600 dark:text-gray-400  py-4 px-10 col-span-12 lg:col-span-5 xl:col-span-4 overflow-y-scroll self-start"
+              style={{
+                // hide scrollbar
+                scrollbarWidth: 'none',
+                msOverflowStyle: 'none',
+              }}
+            >
+              <p className="apply-font-o text-[#1d75c7] text-lg reveal">
+                About us
+              </p>
+              <h1 className="reveal text-xl sm:text-2xl mt-1 mb-4 text-[#69af49] font-extrabold">
+                Who we are and what you <br /> get from our spaces
+              </h1>
+
+              <p
                 style={{
-                  // hide scrollbar
+                  maxHeight: '400px', // Adjust height as needed
+                  overflowY: 'scroll',
                   scrollbarWidth: 'none',
                   msOverflowStyle: 'none',
                 }}
+                className="text-sm reveal border-red-400 m-2 overflow-y-scroll py-5"
               >
-                <p className="apply-font-o text-[#1d75c7] text-lg reveal">
-                  About us
-                </p>
-                <h1 className="reveal text-xl sm:text-2xl mt-1 mb-4 text-[#69af49] font-extrabold">
-                  Who we are and what you <br /> get from our spaces
-                </h1>
-
-                <p
-                  style={{
-                    maxHeight: '400px', // Adjust height as needed
-                    overflowY: 'scroll',
-                    scrollbarWidth: 'none',
-                    msOverflowStyle: 'none',
-                  }}
-                  className="text-sm reveal border-red-400 m-2 overflow-y-scroll py-5"
-                >
-                  {data.about}
-                </p>
-              </div>{' '}
-            </div>
-            {/* accordion for top reasons to study */}
-            <div className="reveal  w-full h-auto px-10">
-              <div className="w-full flex justify-center items-center my-4">
-                <h2 className=" text-black dark:text-white font-extrabold text-2xl text-center">
-                  Why you should study at Level learning center
-                </h2>
-              </div>
-
-              <InfiniteMovingCards
-                items={data.reasons}
-                direction="right"
-                speed="slow"
-                pauseOnHover={true}
-                className="w-full mx-auto"
-              />
-            </div>
+                {data.about}
+              </p>
+            </div>{' '}
           </div>
-        </ScrollAnimateWrapper>
-      </div>
-    </>
+          {/* accordion for top reasons to study */}
+          <div className="reveal  w-full h-auto px-10">
+            <div className="w-full flex justify-center items-center my-4">
+              <h2 className=" text-black dark:text-white font-extrabold text-2xl text-center">
+                Why you should study at Level learning center
+              </h2>
+            </div>
+
+            <InfiniteMovingCards
+              items={data.reasons}
+              direction="right"
+              speed="slow"
+              pauseOnHover={true}
+              className="w-full mx-auto"
+            />
+          </div>
+        </div>
+      </ScrollAnimateWrapper>
+    </div>
   );
 };
 
