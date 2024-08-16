a427a82537edd510b5f6852cc73272ebadea2457
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-16 16:13:54 +0200

    chore: Update metadata for Level Learning Center

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,45 @@ const inter = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: 'fitfabric',
-  description: 'your fitness club',
+  title: 'Level Learning Center',
+  description: 'The place where anything is possible to learn',
+  keywords:
+    'education, learning, online courses, e-learning, tutorials, level-edu',
+  authors: [
+    {
+      name: 'Level Education Center',
+    },
+  ],
+  robots: 'index, follow ',
+  openGraph: {
+    type: 'website',
+    url: 'https://www.level-edu.uz',
+    title: 'Level Learning Center',
+    description: 'The place where anything is possible to learn',
+    images: [
+      {
+        url: '/logo.svg',
+        width: 1200,
+        height: 630,
+        alt: 'Level Learning Center',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '',
+    creator: '',
+    title: 'Level Learning Center',
+    description: 'The place where anything is possible to learn',
+    images: [
+      {
+        url: '/logo.svg',
+        width: 1200,
+        height: 630,
+        alt: 'Level Learning Center',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -31,6 +68,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content={metadata.description || ''} />
+        <meta name="keywords" content={String(metadata.keywords) || ''} />
+        <title>{String(metadata.title)}</title>
+        <link rel="icon" href="./favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={helvetica.className}>{children}</body>
     </html>
   );
