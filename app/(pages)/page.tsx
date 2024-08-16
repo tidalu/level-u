8adf33310f15455740bde21f4e91c40a40f4c6fd
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-16 16:13:44 +0200

    chore: Optimize landing page layout and add lazy loading for images

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -12,29 +12,29 @@ const LandingPage = () => {
       <ScrollAnimateWrapper>
         {/* landing banner */}
         <LandingBanner />
-
-        <div className=" max-w-[1600px] mx-auto reveal">
-          {/* offer */}
-          <Offer />
-          {/* available clubs */}
-          <section>
-            <div className="my-16 px-3 lg:px-16 mx-auto reveal">
-              <h2 className="reveal text-black dark:text-white font-bold text-2xl 2xl:text-3xl text-center">
-                Classes available in our clubs
-              </h2>
-              <p className=" reveal text-[12px] 2xl:text-lg text-center text-gray-500  mt-4">
-                Choose your favorite activities and learn more
-              </p>
-
-              <Categories />
-            </div>
-          </section>
-          {/* popular activities */}
-          <PopulaActivities />
-          {/* yes2move */}
-          {/* <YesMove /> */}
-        </div>
       </ScrollAnimateWrapper>
+
+      <div className=" max-w-[1600px] mx-auto reveal">
+        {/* offer */}
+        <Offer />
+        {/* available clubs */}
+        <section>
+          <div className="my-16 px-3 lg:px-16 mx-auto reveal">
+            <h2 className="reveal text-black dark:text-white font-bold text-2xl 2xl:text-3xl text-center">
+              Classes available in our clubs
+            </h2>
+            <p className=" reveal text-[12px] 2xl:text-lg text-center text-gray-500  mt-4">
+              Choose your favorite activities and learn more
+            </p>
+
+            <Categories />
+          </div>
+        </section>
+        {/* popular activities */}
+        <PopulaActivities />
+        {/* yes2move */}
+        {/* <YesMove /> */}
+      </div>
     </div>
   );
 };
