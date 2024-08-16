69ff3330f2323d41fd2bb379db56c6aa5227bdca
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-16 07:40:21 +0200

    chore: Update image URLs for home banners

--- components/home-blogList.tsx
+++ components/home-blogList.tsx
@@ -8,6 +8,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import ScrollAnimateWrapper from './ScrollAnimateWrapper';
+import ToolTipProv from './ToolTipProv';
 
 const HomeBlogList = () => {
   const settings = {
@@ -65,23 +66,27 @@ const HomeBlogList = () => {
   const BlogList = [
     {
       id: 1,
-      img: '/fit1.webp',
-      title: 'We will introduce you to the world of fitness and gym',
+      img: '/coming-soon.jpg',
+      title: 'Something you want to know about us',
+      comingSoon: true,
     },
     {
       id: 2,
-      img: '/fit2.webp',
-      title: 'We will give you a choice of many types of classes',
+      img: '/coming-soon.jpg',
+      title: 'Something you want to know about us',
+      comingSoon: true,
     },
     {
       id: 3,
-      img: '/fit3.webp',
-      title: 'We will create a professional exercise plan',
+      img: '/coming-soon.jpg',
+      title: 'Something you want to know about us',
+      comingSoon: true,
     },
     {
       id: 4,
-      img: '/fit4.webp',
-      title: "And we'll make sure you want to get up from couch ",
+      img: '/coming-soon.jpg',
+      title: 'Something you want to know about us ',
+      comingSoon: true,
     },
   ];
 
@@ -90,29 +95,31 @@ const HomeBlogList = () => {
       <Slider {...settings} className=" ">
         {BlogList.map((item) => (
           <div key={item.id} className=" h-full">
-              <div className=" bg-white dark:bg-[#020817] rounded-2xl p-4 group/item mr-5">
-                <Link href="/blogs/package-info">
-                  <div className=" grid grid-cols-12 gap-4">
-                    <div className=" col-span-6">
-                      <img src={item.img} className=" rounded-2xl" alt="fit1" />
-                    </div>
-                    <div className=" col-span-6 flex flex-col justify-between">
-                      <div className=" text-[13px] 2xl:text-lg font-semibold leading-6 group-hover/item:underline">
-                        {item.title}
-                      </div>
-
-                      <div className=" text-right">
-                        <Button
-                          variant="primaryGreen"
-                          className=" rounded-full p-0 h-5 px-2"
-                        >
-                          <ChevronRight size={14} className="text-black" />
-                        </Button>
-                      </div>
-                    </div>
+            <div className=" bg-white dark:bg-[#020817] rounded-2xl p-4 group/item mr-5">
+              <div className=" grid grid-cols-12 gap-4">
+                <div className=" col-span-6">
+                  <img
+                    src={item.img}
+                    className=" rounded-2xl object-cover w-full h-32"
+                    alt="fit1"
+                  />
+                </div>
+                <div className=" col-span-6 flex flex-col justify-between">
+                  <div className=" text-[13px] 2xl:text-lg font-semibold leading-6 group-hover/item:underline">
+                    {item.title}
                   </div>
-                </Link>
+
+                  <div className=" text-right">
+                    <Button
+                      variant="primaryGreen"
+                      className=" rounded-full p-0 h-5 px-2"
+                    >
+                      <ChevronRight size={14} className="text-black" />
+                    </Button>
+                  </div>
+                </div>
               </div>
+            </div>
           </div>
         ))}
       </Slider>
