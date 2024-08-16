76cb959f6ec715de7d17151a2e8d9e26a1c435ba
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-16 07:39:24 +0200

    chore: Update social media icons in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import FooterLinks from './footer-links';
+import { FaTelegram, FaInstagram, FaLink } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -10,61 +11,28 @@ const Footer = () => {
         </div>
         <FooterLinks />
 
-        <div className="reveal grid grid-cols-12 md:gap-20 text-sm text-gray-300 px-6 py-16 md:px-3">
-          <div className=" col-span-full md:col-span-4 ">
-            <h3 className=" text-white font-semibold">Benefit Systems SA,</h3>
-            <p className="mb-4">ul. Plac Europejski 2, 00-844 Warsaw</p>
-
-            <div className=" leading-6 break-words">
-              District Court for the Capital City of Warsaw in Warsaw, 13th
-              Commercial Division of the National Court Register, KRS:
-              0000370919, NIP: 8361676510, BDO: 000558784. Share capital PLN
-              2,958,292.00.{' '}
-            </div>
-          </div>
-          <div className=" col-span-full md:col-span-4 mt-10 md:mt-0">
-            <div>Personal Data Protection Inspector Kinga Bieniek-Zawadzka</div>
-            <Link
-              className=" text-white underline font-bold"
-              href="mailto:iod@benefitsystems.pl"
-            >
-              iod@benefitsystems.pl
-            </Link>
-          </div>
-        </div>
-
-        <div className="reveal flex flex-col gap-5 md:flex-row justify-between items-center px-6 md:px-3">
+        <div className="reveal flex flex-col gap-5  mt-6 md:flex-row justify-between items-center px-6 md:px-3">
           <div className=" text-sm text-gray-300">
-            © Copyright 2024 Benefit Systems S.A.
+            © Copyright 2024 Level learning center
           </div>
-          <div className="flex items-center gap-6">
+          <div className="flex items-center gap-6 ">
             <Link
               href="/"
               className="border border-gray-600 rounded-full block"
             >
-              <img
-                src="/facebook.svg"
-                width={20}
-                alt="facebook"
-                className="m-2"
-              />
+              <FaTelegram size={20} className="m-2" />
             </Link>
             <Link
               href="/"
               className="border border-gray-600 rounded-full block"
             >
-              <img
-                src="/instagram.svg"
-                width={20}
-                alt="instagram"
-                className="m-2"
-              />
+              <FaInstagram size={20} className="m-2" />
             </Link>
             <Link
               href="/"
               className="border border-gray-600 rounded-full block"
             >
-              <img src="/tiktok.svg" width={20} alt="tiktok" className="m-2" />
+              <FaLink size={20} className="m-2" />
             </Link>
           </div>
         </div>
