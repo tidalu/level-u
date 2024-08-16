b84607597a5ce0e0c4b80f711a49fedd9296fa64
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-16 07:36:10 +0200

    chore: Update StudyAbroad page layout with Hero component

--- app/(pages)/(routes)/study-abroad/page.tsx
+++ app/(pages)/(routes)/study-abroad/page.tsx
@@ -1,7 +1,18 @@
+import Hero from '@/components/hero';
+import DotPattern from '@/components/magicui/dot-pattern';
+import { cn } from '@/lib/utils';
 import React from 'react';
 
 function StudyAbroad() {
-  return <div className=" max-w-screen-lg mx-auto px-3 pt-36 mb-28"></div>;
+  return (
+    <div className=" mx-auto px-3 pt-36 mb-28 max-w-screen-lg">
+      <div className="relative flex min-h-screen flex-col py-20 lg:py-0 md:py-0 sm:py-20">
+        <div className="flex-1">
+          <Hero />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default StudyAbroad;
