b00cc77206b0abb35f2db3f8ee0f284aa8353b71
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-16 16:13:31 +0200

    chore: Update study abroad page layout and styling

--- app/(pages)/(routes)/study-abroad/page.tsx
+++ app/(pages)/(routes)/study-abroad/page.tsx
@@ -1,12 +1,10 @@
 import Hero from '@/components/hero';
-import DotPattern from '@/components/magicui/dot-pattern';
-import { cn } from '@/lib/utils';
 import React from 'react';
 
 function StudyAbroad() {
   return (
-    <div className=" mx-auto px-3 pt-36 mb-28 max-w-screen-lg">
-      <div className="relative flex min-h-screen flex-col py-20 lg:py-0 md:py-0 sm:py-20">
+    <div className=" mx-auto px-3 pt-36 mb-16  max-w-screen-lg">
+      <div className="relative flex  flex-col py-20 lg:py-0 md:py-0 sm:py-20">
         <div className="flex-1">
           <Hero />
         </div>
