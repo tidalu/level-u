3f173dd664446959c402525ec0ad84a01a3a6e72
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-17 17:11:48 +0200

    refactor: Update DockLive component styles for responsiveness

--- components/magicui/dock-live.tsx
+++ components/magicui/dock-live.tsx
@@ -6,7 +6,7 @@ export type IconProps = React.HTMLAttributes<SVGElement>;
 
 export default function DockLive() {
   return (
-    <div className="  h-[500px] w-full max-w-[32rem] flex-col items-center justify-center overflow-hidden rounded-lg  bg-background ">
+    <div className="  h-auto w-full max-w-[32rem] flex-col items-center justify-center overflow-hidden rounded-lg  bg-background ">
       {/* <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
         Dock
       </span> */}
