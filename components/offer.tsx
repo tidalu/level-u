5430f874edd9d29dda4c6e22781c0758b09f4eb7
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-17 17:16:36 +0200

    refactor: Update Marquee3D component to remove unused code and optimize image loading

--- components/offer.tsx
+++ components/offer.tsx
@@ -5,13 +5,15 @@ import React from 'react';
 import { Button } from './ui/button';
 import ScrollAnimateWrapper from './ScrollAnimateWrapper';
 import ToolTipProv from './ToolTipProv';
+import { useLocalizedData } from '@/lib/useLocalizedData';
 
 const Offer = () => {
+  const data = useLocalizedData();
   return (
     <section>
       <div className="my-16 px-3 lg:px-16 mx-auto">
         <h2 className="text-black dark:text-white font-extrabold text-2xl text-center">
-          Offer
+          {data.offers.title}
         </h2>
 
         <div
@@ -22,17 +24,17 @@ const Offer = () => {
             <div className="grid grid-cols-1 sm:grid-cols-2">
               <div className="pl-10 py-10 md:py-5 flex flex-col justify-center text-sm 2xl:text-xl">
                 <div className="text-black dark:text-white font-bold mb-5">
-                  for you
+                  {data.offers.forYou.title}
                 </div>
                 <p className="text-[12px] 2xl:text-lg leading-7 text-gray-500">
-                  Choose a center and start your adventure with us!
+                  {data.offers.forYou.desc}
                 </p>
 
                 <Button
                   variant="primaryGreen"
                   className="mt-12 dark:text-black shadow-xl w-fit rounded-full text-sm 2xl:text-lg"
                 >
-                  Choose a center
+                  {data.offers.forYou.btn}
                 </Button>
               </div>
 
@@ -48,10 +50,18 @@ const Offer = () => {
           >
             <div className="grid grid-cols-1 sm:grid-cols-2">
               <div className="pl-10 py-10 md:py-5 flex flex-col justify-center text-sm 2xl:text-xl">
-                <div className="text-black font-bold mb-5">for companies</div>
+                <div className="text-black font-bold mb-5">
+                  {data.offers.forCompanies.title}
+                </div>
                 <p className="text-[12px] 2xl:text-lg leading-7 text-gray-500">
-                  Check out our offers for <br />
-                  your teams and employees
+                  {data.offers.forCompanies.desc.map(
+                    (item: string, index: number) => (
+                      <React.Fragment key={index}>
+                        {item}
+                        {index !== 1 && <br />}
+                      </React.Fragment>
+                    )
+                  )}
                 </p>
                 <div className="mt-6 dark:text-black ">
                   <ToolTipProv content="coming soon">
@@ -59,7 +69,7 @@ const Offer = () => {
                       variant="primaryGreen"
                       className=" w-fit rounded-full text-sm 2xl:text-lg shadow-xl"
                     >
-                      Find out more
+                      {data.offers.forCompanies.btn}
                     </Button>
                   </ToolTipProv>
                 </div>
@@ -79,10 +89,12 @@ const Offer = () => {
                 <img src="/offer3.webp" className="rounded-2xl" alt="" />
               </div>
               <div className="text-black dark:text-white relative z-10 mt-12 text-sm mb-2">
-                <span className="font-bold">IELTS</span>
+                <span className="font-bold">
+                  {data.offers.packages.ielts.title}
+                </span>
               </div>
               <div className="flex justify-between relative z-10 text-[12px] text-gray-500">
-                Online training
+                {data.offers.packages.ielts.desc}
                 <Button
                   variant="primaryGreen"
                   className="rounded-full p-0 h-5 px-2"
@@ -106,11 +118,11 @@ const Offer = () => {
               </div>
               <div className="text-black dark:text-white relative z-10 mt-12 text-sm mb-2">
                 <span className="font-bold group-hover:item:underline">
-                  Korean language
+                  {data.offers.packages.korean.title}
                 </span>
               </div>
               <div className="flex justify-between relative z-10 text-[12px] text-gray-500">
-                Online training
+                {data.offers.packages.ielts.desc}
                 <Button
                   variant="primaryGreen"
                   className="rounded-full p-0 h-5 px-2"
@@ -134,11 +146,11 @@ const Offer = () => {
               </div>
               <div className="text-black dark:text-white relative z-10 mt-12 text-sm mb-2">
                 <span className="font-bold group-hover:item:underline">
-                  Kids club
+                  {data.offers.packages.kids.title}
                 </span>
               </div>
               <div className="flex justify-between relative z-10 text-[12px] text-gray-500">
-                Online training
+                {data.offers.packages.kids.desc}
                 <Button
                   variant="primaryGreen"
                   className="rounded-full p-0 h-5 px-2"
