c611de10dae5a4af71ad2df5ce05b4a42083b062
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-17 17:13:04 +0200

    refactor: Update FooterLinks component to use localized data for dynamic rendering

--- components/footer-links.tsx
+++ components/footer-links.tsx
@@ -1,14 +1,15 @@
 'use client';
 
+import { useLocalizedData } from '@/lib/useLocalizedData';
 import { cn } from '@/lib/utils';
 import { ChevronDown } from 'lucide-react';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
-import ScrollAnimateWrapper from './ScrollAnimateWrapper';
 
 const FooterLinks = () => {
   const [isMobile, setIsMobile] = useState(false);
   const [openSection, setOpenSection] = useState(null);
+  const data = useLocalizedData();
 
   useEffect(() => {
     const handleResize = () => setIsMobile(window.innerWidth < 768);
@@ -38,7 +39,7 @@ const FooterLinks = () => {
         onClick={() => toggleSection('information')}
       >
         <h3 className="cursor-pointer md:cursor-default flex justify-between items-center font-bold">
-          INFORMATION
+          {data?.footer?.information.title}
           <ChevronDown className={cn('block md:hidden text-[#6cce40]')} />
         </h3>
         <ul
@@ -48,26 +49,16 @@ const FooterLinks = () => {
               : 'max-h-0'
           }`}
         >
-          <li className="py-2">
-            <Link href="/" className="hover:underline">
-              Level education
-            </Link>
-          </li>
-          <li className="py-2">
-            <Link href="/careers" className="hover:underline">
-              Careers
-            </Link>
-          </li>
-          <li className="py-2">
-            <Link href="/#news-section" className="hover:underline ">
-              News
-            </Link>
-          </li>
-          <li className="py-2">
-            <Link href="/classes" className="hover:underline">
-              Classes
-            </Link>
-          </li>
+          {data?.footer?.information.links.map((link: any, index: number) => (
+            <li key={index} className="py-2">
+              <Link
+                href={link.href ? link.href : '/'}
+                className="hover:underline"
+              >
+                {link.title}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
       <div
@@ -75,7 +66,7 @@ const FooterLinks = () => {
         onClick={() => toggleSection('services')}
       >
         <h3 className="cursor-pointer md:cursor-default flex justify-between items-center font-bold">
-          SERVICES
+          {data?.footer?.services.title}
           <ChevronDown className={cn('block md:hidden text-[#6cce40]')} />
         </h3>
         <ul
@@ -85,56 +76,25 @@ const FooterLinks = () => {
               : 'max-h-0'
           }`}
         >
-          <li className="py-2">
-            <Link href="/#offersComp" className="hover:underline">
-              Offer for companies
-            </Link>
-          </li>
-          <li className="py-2">
-            <Link href="/#forYou" className="hover:underline">
-              For you
-            </Link>
-          </li>
-          <li className="py-2">
-            <Link href="/study-abroad" className="hover:underline">
-              Study Abroad
-            </Link>
-          </li>
-          <li className="py-2">
-            <Link href="/contact#map" className="hover:underline">
-              Find your center
-            </Link>
-          </li>
+          {data?.footer?.services.links.map((link: any, index: number) => (
+            <li key={index} className="py-2">
+              <Link
+                href={link.href ? link.href : '/'}
+                className="hover:underline"
+              >
+                {link.title}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
-      {/* <div
-        className="col-span-12 md:col-span-3 lg:col-span-2 px-6 md:px-3 py-2"
-        onClick={() => toggleSection('documents')}
-      >
-        <h3 className="cursor-pointer md:cursor-default flex justify-between items-center font-bold">
-          DOCUMENTS
-          <ChevronDown className={cn('block md:hidden text-[#6cce40]')} />
-        </h3>
-        <ul
-          className={`transition-all text-gray-300 text-sm duration-300 ease-in-out overflow-hidden md:overflow-visible ${
-            openSection === 'documents' || !isMobile
-              ? 'max-h-screen md:max-h-none'
-              : 'max-h-0'
-          }`}
-        >
-          <li className="py-2">
-            <Link href="/files-to-download" className="hover:underline">
-              Files to download
-            </Link>
-          </li>
-        </ul>
-      </div> */}
+
       <div
         className="col-span-12 md:col-span-3 lg:col-span-2 px-6 md:px-3 py-2"
         onClick={() => toggleSection('help')}
       >
         <h3 className="cursor-pointer md:cursor-default flex justify-between items-center font-bold">
-          Get in touch
+          {data?.footer?.contact.title}
           <ChevronDown className={cn('block md:hidden text-[#6cce40]')} />
         </h3>
         <ul
@@ -144,11 +104,16 @@ const FooterLinks = () => {
               : 'max-h-0'
           }`}
         >
-          <li className="py-2">
-            <Link href="/contact" className="hover:underline">
-              Contact
-            </Link>
-          </li>
+          {data?.footer?.contact.links.map((link: any, index: number) => (
+            <li key={index} className="py-2">
+              <Link
+                href={link.href ? link.href : '/'}
+                className="hover:underline"
+              >
+                {link.title}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
