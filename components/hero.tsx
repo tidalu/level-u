6f7066463b0f75992b4e6c23734bf9aa70839b7a
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-17 17:15:05 +0200

    refactor: Update Hero component to use localized data for dynamic rendering

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 import DockLive from './magicui/dock-live';
 import { Button } from './ui/button';
 import { grid } from 'ldrs';
+import { useLocalizedData } from '@/lib/useLocalizedData';
+import { toast } from 'react-toastify';
 
 const Hero = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -11,6 +13,7 @@ const Hero = () => {
     phone: '',
   });
   const [isLoading, setIsLoading] = useState(false);
+  const data = useLocalizedData();
 
   useEffect(() => {
     // Register the grid only on the client side
@@ -38,7 +41,7 @@ const Hero = () => {
         phone: '',
       });
     } catch (error) {
-      alert('Error submitting form better to send an email');
+      toast.error(data.toastMessages.error);
     } finally {
       setIsLoading(false);
     }
@@ -56,19 +59,10 @@ const Hero = () => {
       <section className="flex flex-col items-center justify-center space-y-5 sm:space-y-10">
         <div className="w-full text-center px-4 sm:px-0">
           <h1 className="text-2xl sm:text-4xl lg:text-6xl font-semibold leading-tight text-neutral-900 dark:text-neutral-100">
-            Join Our Waitlist: Be Among the First to Explore the World!
+            {data.hero.title}
           </h1>
-          <p className="mt-4 text-lg sm:text-xl text-neutral-600 dark:text-neutral-300">
-            Our study abroad programs are designed to offer you a unique
-            educational experience that combines academic excellence with
-            cultural immersion. Prepare to discover new cultures, meet diverse
-            communities, and expand your global perspective while pursuing your
-            studies. This landing page is specially crafted to{' '}
-            <b>save you time</b> and help you find the{' '}
-            <b>perfect international program</b> before you embark on this
-            exciting journey. Join our waitlist today and be the first to
-            receive updates on our upcoming programs, scholarships, and
-            exclusive opportunities!
+          <p className="mt-4 text-lg sm:text-xl text-wrap text-neutral-600 dark:text-neutral-300">
+            {data.hero.description}
           </p>
           {!isSubmitted && !isLoading ? (
             <form
@@ -82,12 +76,13 @@ const Hero = () => {
                   className=" text-[10px] text-gray-500 p-1 absolute -top-3 left-6  bg-white dark:text-gray-400 dark:bg-[#020817]"
                   htmlFor="name"
                 >
-                  <span className=" text-red-400">*</span>Full Name
+                  <span className=" text-red-400">*</span>
+                  {data.contactForm.name}
                 </label>
                 <input
                   type="text"
                   className=" w-full l lg:max-w-[400px] sm:w-[300px] md:w-[350px] lg:w-[400px]  dark:bg-[#020817] outline-none text-sm border-2 border-[#dbdbdb] rounded-xl py-4 px-6"
-                  placeholder="Your full name"
+                  placeholder={data.contactForm.namePlaceholder}
                   required
                   name="name"
                   onChange={(e) =>
@@ -100,12 +95,13 @@ const Hero = () => {
                   className=" text-[10px] text-gray-500 p-1 absolute -top-3 left-6  bg-white dark:text-gray-400 dark:bg-[#020817]"
                   htmlFor="phone"
                 >
-                  <span className=" text-red-400">*</span>Telephone
+                  <span className=" text-red-400">*</span>
+                  {data.contactForm.phone}
                 </label>
                 <input
                   type="tel"
                   className=" w-full l lg:max-w-[400px] sm:w-[300px] md:w-[350px] lg:w-[400px]  dark:bg-[#020817] outline-none text-sm border-2 border-[#dbdbdb] rounded-xl py-4 px-6"
-                  placeholder="+998 99 999 99 99"
+                  placeholder={data.contactForm.phonePlaceholder}
                   required
                   name="phone"
                   onChange={(e) =>
@@ -118,7 +114,7 @@ const Hero = () => {
                 className="w-full lg:max-w-[400px] sm:w-[300px] md:w-[350px] lg:w-[400px]"
                 size="lg"
               >
-                Submit
+                {data.hero.button}
               </Button>
             </form>
           ) : isLoading && !isSubmitted ? (
@@ -128,16 +124,13 @@ const Hero = () => {
           ) : isSubmitted ? (
             <div className="w-full flex flex-col items-center justify-center p-5 rounded-lg bg-green-100 dark:bg-green-800 mt-5">
               <h2 className="text-2xl sm:text-3xl font-semibold text-green-800 dark:text-green-200">
-                Thank You for Joining!
+                {data.hero.thankYou.title}
               </h2>
               <p className="mt-2 text-lg text-green-700 dark:text-green-300">
-                We appreciate your interest in our programs. You will be among
-                the first to receive updates on our new study abroad
-                opportunities.
+                {data.hero.thankYou.description}
               </p>
               <p className="mt-4 text-sm text-neutral-600 dark:text-neutral-300">
-                Stay tuned for more information. Follow us on our social media
-                channels for the latest news and updates.
+                {data.hero.thankYou.stayTuned}
               </p>
             </div>
           ) : (
@@ -146,7 +139,7 @@ const Hero = () => {
         </div>
         <div className="flex flex-col items-center justify-center gap-4 w-full mx-auto py-2 max-w-full">
           <p className=" text-sm sm:text-lg text-neutral-600 dark:text-neutral-300">
-            Check us out on social media ⚡️
+            {data.hero.checkUs} ⚡️
           </p>
           <DockLive />
         </div>
