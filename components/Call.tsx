5e7b7222d572d0096cc5377a012d30644b628dbf
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-19 05:49:06 +0200

    refactor: Improve scroll behavior in Call component

--- components/Call.tsx
+++ components/Call.tsx
@@ -1,18 +1,43 @@
-import React from 'react';
-//phone icon
+import React, { useEffect, useState, useRef } from 'react';
 import { Phone } from 'lucide-react';
 
 function Call() {
+  const [scrollActive, setScrollActive] = useState(true);
+  const scrollTimeoutRef = useRef<null | NodeJS.Timeout>(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollActive(false);
+
+      if (scrollTimeoutRef.current) {
+        clearTimeout(scrollTimeoutRef.current);
+      }
+
+      scrollTimeoutRef.current = setTimeout(() => {
+        setScrollActive(true);
+      }, 3000); // Changed to 3 seconds for visibility
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      if (scrollTimeoutRef.current) {
+        clearTimeout(scrollTimeoutRef.current);
+      }
+    };
+  }, []);
+
   return (
     <div
-      className="transition-all delay-75 z-20 bounce rounded-full w-14 h-14 right-5 bottom-5 filter hover:invert fixed
-        bg-[#6cce40] dark:bg-[#223563] flex justify-center items-center shadow-2xl cursor-pointer
-        "
-      onClick={() => {
-        window.open('tel:+998559033747');
-      }}
+      className={`transition-all delay-150 ease-linear z-20 rounded-full fixed bottom-5 right-5 filter hover:invert bg-[#6cce40] dark:bg-[#223563] flex justify-center items-center shadow-2xl cursor-pointer ${
+        scrollActive ? 'opacity-100' : 'opacity-0'
+      } ${
+        scrollActive ? 'translate-x-0' : 'translate-x-32'
+      } w-10 h-10  lg:w-12 lg:h-12 xl:w-14 xl:h-14`}
+      onClick={() => window.open('tel:+998559033747')}
     >
-      <Phone size={24} />
+      <Phone size={0} className="w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8 " />
     </div>
   );
 }
