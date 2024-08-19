13264b0c1a6d0c2657952b029e78a79aaa5f2b82
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-19 05:48:02 +0200

    refactor: Update DockLive component with clickable social media icons

--- components/magicui/dock-live.tsx
+++ components/magicui/dock-live.tsx
@@ -6,19 +6,22 @@ export type IconProps = React.HTMLAttributes<SVGElement>;
 
 export default function DockLive() {
   return (
-    <div className="  h-auto w-full max-w-[32rem] flex-col items-center justify-center overflow-hidden rounded-lg  bg-background ">
+    <div className="  h-auto w-full max-w-[32rem] flex-col items-center justify-center overflow-hidden rounded-lg  bg-transparent ">
       {/* <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
         Dock
       </span> */}
       <Dock>
         <DockIcon>
-          <FaInstagram className="h-6 w-6" />
+          <FaInstagram
+            className="h-6 w-6"
+            onClick={() => window.open('https://t.me/level_edu_uz')}
+          />
         </DockIcon>
         <DockIcon>
-          <FaTelegram className="h-6 w-6" />
-        </DockIcon>
-        <DockIcon>
-          <FaLink className="h-6 w-6" />
+          <FaTelegram
+            className="h-6 w-6"
+            onClick={() => window.open('https://instagram.com/level_edu_uz')}
+          />
         </DockIcon>
       </Dock>
     </div>
