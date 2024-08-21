b9ea1e3457462f3f50b0f788c29e1d5c934a2169
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-21 19:44:14 +0200

    refactor: Update Careers page to fix broken link and improve UI

--- app/(pages)/(routes)/careers/page.tsx
+++ app/(pages)/(routes)/careers/page.tsx
@@ -78,8 +78,8 @@ function Careers() {
                         style={{ opacity: 1, transform: 'none' }}
                       >
                         <a
-                          className="reveal flex cursor-pointer items-center justify-between group careers_posting__ypMs4"
-                          href="/careers"
+                          className="reveal flex cursor-pointer items-center justify-between group "
+                          href="/contact"
                         >
                           <div>
                             <h2 className="text-lg md:text-2xl">
@@ -262,6 +262,8 @@ function RecruitmentProcess({
   title: string;
 }) {
   const [currentOnScreen, setCurrentOnScreen] = useState(data[0]);
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [selectedStepIndex, setSelectedStepIndex] = useState(0);
 
   // Update the currentOnScreen when the data changes (e.g., on language change)
   useEffect(() => {
@@ -269,12 +271,22 @@ function RecruitmentProcess({
   }, [data]);
 
   const handleClick = (index: number) => {
+    setSelectedStepIndex(index);
     setCurrentOnScreen(data[index]);
+    setIsAnimating(true);
   };
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setIsAnimating(false), 500);
+    return () => clearTimeout(timeoutId);
+  }, [currentOnScreen]);
 
   return (
     <div className="flex flex-col-reverse lg:flex-row gap-10 lg:gap-4 mx-auto px-5">
-      <div className="rounded-xl reveal flex flex-col basis-1/2 mx-5 border-gray-900 dark:border-gray-200 border">
+      <div
+        className={` rounded-xl reveal flex flex-col basis-1/2 mx-5 border-gray-900 dark:border-gray-200 border ${
+          isAnimating ? 'slide-in-elliptic-top-fwd' : 'animation-done'
+        }`}
+      >
         <div className="basis-1/2">
           <video
             src={currentOnScreen.stepVideo}
@@ -300,8 +312,10 @@ function RecruitmentProcess({
               <div
                 key={index}
                 onClick={() => handleClick(index)}
-                className="px-10 py-6 rounded-full hover:bg-gray-800 hover:text-white focus:bg-gray-800 focus:text-white text-gray-800 dark:text-white dark:hover:bg-white dark:hover:text-gray-800 cursor-pointer border border-gray-800 dark:border-white flex gap-4
-                dark:focus:bg-white dark:focus:text-gray-800 reveal"
+                className={`px-10 py-6 rounded-full hover:bg-gray-800 hover:text-white focus:bg-gray-800 focus:text-white text-gray-800 dark:text-white dark:hover:bg-white dark:hover:text-gray-800 cursor-pointer border border-gray-800 dark:border-white flex gap-4 ${
+                  selectedStepIndex === index ? 'bg-gray-800 text-white' : ''
+                }
+  dark:focus:bg-white dark:focus:text-gray-800 reveal`}
               >
                 <p className="text-xl font-bold">0{step.stepIndex}</p>
                 <p className="text-xl ">{step.stepTitle}</p>
