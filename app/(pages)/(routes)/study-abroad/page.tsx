f933ddce73806d6dfb9e24445fc0e8d53dbca76c
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-21 15:29:11 +0200

    refactor: Update StudyAbroad page to use Hero component instead of GlobeDemo

--- app/(pages)/(routes)/study-abroad/page.tsx
+++ app/(pages)/(routes)/study-abroad/page.tsx
@@ -1,4 +1,3 @@
-import { GlobeDemo } from '@/components/Globe';
 import Hero from '@/components/hero';
 import React from 'react';
 
@@ -7,9 +6,7 @@ function StudyAbroad() {
     <div className=" mx-auto px-3 pt-36 mb-16  max-w-screen-lg">
       <div className="relative flex  flex-col py-20 lg:py-0 md:py-0 sm:py-20">
         <div className="flex-1">
-          <GlobeDemo>
-            <Hero />
-          </GlobeDemo>
+          <Hero />
         </div>
       </div>
     </div>
--- components/categories.tsx
+++ components/categories.tsx
@@ -30,35 +30,62 @@ const Categories = () => {
   const data = useLocalizedData();
   return (
     <div>
-        <div className="flex flex-col items-center gap-7 mt-6 md:flex-row flex-wrap justify-evenly">
-          {Object.values(data.classPage.classList as ClassList[]).map(
-            (item: ClassList, index: number) => {
-              return (
-                <div className="" key={index}>
-                  <ul className="reveal bg-white custom-shadow dark:bg-[#86868517] width-[60%] dark:bg-[#020817]  lg:w-auto p-10 rounded-xl text-sm 2xl:text-lg">
-                    <li className=" text-gray-400 font-semibold mb-3">
-                      {item.title}
-                    </li>
-                    {item.list.map((item: Class) => (
-                      <li className="mb-1" key={item.name}>
-                        <Link
-                          href={`/classess/${item.name.toLowerCase()}`}
-                          className="hover:underline text-[#3d4939] dark:text-gray-300 text-[11px] 2xl:text-[14px] font-extrabold"
-                        >
-                          {item.name}
-                          <ChevronRight
-                            size={12}
-                            className=" inline-block  text-[#6cce40]"
+      <div className="flex flex-col items-center  mt-6 md:flex-row flex-wrap justify-between">
+        {Object.values(data.classPage.classList as ClassList[]).map(
+          (item: ClassList, index: number) => {
+            return (
+              <div className="basis-[45%]" key={index}>
+                <ul className="reveal bg-white custom-shadow  dark:bg-[#86868517]  dark:bg-[#020817]  lg:w-auto p-10 rounded-xl text-sm 2xl:text-lg">
+                  <li className=" text-gray-400 font-semibold mb-3">
+                    {item.title}
+                  </li>
+                  {item.list.map((item: Class) => (
+                    <li className="mb-2 h-auto" key={item.name}>
+                      <div
+                        key={item.name}
+                        className="shadow-xl relative rounded-xl pt-32 md:pt-4 p-4 min-w-64 md:min-w-fit md:pl-32 flex-1 md:h-32 xl:h-52 overflow-hidden dark:bg-[#0f1227]"
+                      >
+                        <div className="cursor-pointer">
+                          <div className="absolute skew top-0 left-0 right-0 bottom-52 md:bottom-0  w-full h-full overflow-hidden">
+                            <img
+                              src="/offer3.webp"
+                              className="rounded-2xl object-cover w-full h-full"
+                              alt=""
+                            />
+                          </div>
+                          <div className="text-black dark:text-white relative z-10 mt-12 text-sm mb-2">
+                            <span className="font-bold">{item.name}</span>
+                          </div>
+                          <div className="flex justify-between relative z-10 text-[12px] text-gray-500">
+                            {'short description here'}
+                            <Button
+                              variant="primaryGreen"
+                              className="rounded-full p-0 h-5 px-2"
+                              onClick={() =>
+                                window.open(
+                                  `/classess/${item.name.toLowerCase()}`
+                                )
+                              }
+                            >
+                              <ChevronRight size={14} className="text-black" />
+                            </Button>
+                          </div>
+                          <img
+                            className="absolute z-10 top-0 right-3"
+                            width={50}
+                            src="/offericon-3.webp"
+                            alt=""
                           />
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              );
-            }
-          )}
-        </div>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            );
+          }
+        )}
+      </div>
     </div>
   );
 };
