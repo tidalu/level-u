180c6b2c63293c615bda2454e707576e1dcf3613
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-22 16:37:54 +0200

    refactor: Remove unnecessary border styling in study abroad page

--- app/(pages)/(routes)/study-abroad/page.tsx
+++ app/(pages)/(routes)/study-abroad/page.tsx
@@ -5,7 +5,7 @@ import Globe from '@/components/magicui/globe';
 function StudyAbroad() {
   return (
     <div className=" relative  mx-auto px-3 pt-36 mb-16  max-w-screen-lg">
-      <div className="border  py-20 lg:py-0 md:py-0 sm:py-20">
+      <div className="  py-20 lg:py-0 md:py-0 sm:py-20">
         <Hero />
       </div>
     </div>
