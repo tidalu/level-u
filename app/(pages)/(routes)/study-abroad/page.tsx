c1ff03b4c1d8a64d10b3d5ba1ed6776b02214f88
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-22 12:25:33 +0200

    refactor: Update study abroad page layout and styling

--- app/(pages)/(routes)/study-abroad/page.tsx
+++ app/(pages)/(routes)/study-abroad/page.tsx
@@ -4,8 +4,8 @@ import Globe from '@/components/magicui/globe';
 
 function StudyAbroad() {
   return (
-    <div className=" mx-auto px-3 pt-36 mb-16  max-w-screen-lg">
-      <div className="  py-20 lg:py-0 md:py-0 sm:py-20">
+    <div className=" relative  mx-auto px-3 pt-36 mb-16  max-w-screen-lg">
+      <div className="border  py-20 lg:py-0 md:py-0 sm:py-20">
         <Hero />
       </div>
     </div>
