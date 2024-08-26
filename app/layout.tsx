7abeb9799c25c77fc73f51b87e1932ca7322fe4f
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-26 14:19:34 +0200

    refactor: Update lazy loading and image size in landing-banner.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,14 +4,11 @@ import localFont from 'next/font/local';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Analytics } from '@vercel/analytics/react';
 
-
-
 const helvetica = localFont({
   src: './fonts/helveticaneue-medium.woff2',
   display: 'swap',
   variable: '--font-helvetica',
 });
-
 
 export const metadata: Metadata = {
   title: 'Level Learning Center',
@@ -32,13 +29,6 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
         <link rel="apple-touch-icon" href="/favicon.svg" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          rel="stylesheet"
-          as="style"
-        />
         <link rel="preload" href="/logo.svg" as="image/svg+xml"></link>
         <link
           rel="preload"
