27eff82d9f0415c7b9f5b2242d8813b4c0a2dd5e
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-26 13:08:57 +0200

    refactor: Update next.config.js for image optimization and bundle analysis
    This commit updates the next.config.js file to optimize image loading by configuring remotePatterns for the Next.js Image component. It also adds the @next/bundle-analyzer package to enable bundle analysis for better understanding of the project's dependencies and performance. These changes improve the overall performance and user experience of the application.

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'react-icons'],
+    serverComponentsExternalPackages: [
+      'eslint',
+      'prettier',
+      'typescript',
+      'postcss',
+      'sharp',
+      'typescript',
+    ],
+  },
+  
+};
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
