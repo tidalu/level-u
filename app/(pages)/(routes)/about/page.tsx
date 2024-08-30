6787408818d8ea3c27b8c80a6cb97242f5aa2104
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-30 16:11:31 +0200

    refactor: Remove ThreeDTextScene from AboutPage and add video background

--- app/(pages)/(routes)/about/page.tsx
+++ app/(pages)/(routes)/about/page.tsx
@@ -2,7 +2,6 @@
 import { useLocalizedData } from '@/lib/useLocalizedData';
 import InfiniteMovingCards from '@/components/InfiniteMovingCards';
 import ScrollAnimateWrapper from '@/components/ScrollAnimateWrapper';
-import ThreeDTextScene from '@/components/ThreeDTextScene';
 
 const AboutPage = () => {
   const data = useLocalizedData();
@@ -13,11 +12,21 @@ const AboutPage = () => {
         <ScrollAnimateWrapper>
           <div className="min-h-[800px] h-auto flex flex-col items-center  justify-center pt-32 pb-12 rounded-b-3xl px-3 lg:px-16 ">
             <div className=" relative rounded-2xl   lg:h-auto  grid grid-cols-12 bg-white  dark:bg-[#020817]  my-4 lg:my-10">
-              <div className=" relative col-span-12 reveal lg:col-span-7 xl:col-span-8 h-[300px] lg:h-auto flex xl:justify-end justify-center items-center overflow-hidden bg-slate-200">
-                <ThreeDTextScene />
-                <span className="  absolute bottom-4 left-0 flex w-full justify-center items-center">
-                  <p className=" text-center before:contents  mx-auto py-2 px-4 relative rounded-xl z-10 text-wrap w-auto text-with-blur">
-                    {data?.about?.motto}
+              <div className=" relative col-span-12 reveal rounded-xl lg:col-span-7 xl:col-span-8 h-[300px] lg:h-auto flex xl:justify-end justify-center items-center overflow-hidden bg-slate-200">
+                {/* video */}
+                <video
+                  autoPlay
+                  muted
+                  loop
+                  className="w-full h-full object-cover"
+                >
+                  <source src="/video/about-page.mp4" type="video/mp4" />
+                </video>
+                <span className="  absolute top-4 -left-[25%] md:-left-[30%] xl:-left-[35%] flex w-full justify-center items-center">
+                  <p className=" text-center before:contents   mx-auto py-2 px-4 relative rounded-xl z-10 text-wrap w-auto text-with-blur">
+                    {data?.about?.motto.split(',')[0]}
+                    <br />
+                    {data?.about?.motto.split(',')[1]}
                   </p>
                 </span>
               </div>{' '}
@@ -38,7 +47,7 @@ const AboutPage = () => {
 
                 <p
                   style={{
-                    maxHeight: '400px', 
+                    maxHeight: '400px',
                     overflowY: 'scroll',
                     scrollbarWidth: 'none',
                     msOverflowStyle: 'none',
