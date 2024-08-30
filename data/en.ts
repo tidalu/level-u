3d9978b61572d9eb1ff2aa4b7225815249fb78fc
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-08-30 16:12:34 +0200

    refactor: Update language translations for Uzbek, Russian, and English

--- data/en.ts
+++ data/en.ts
@@ -3,7 +3,7 @@ export const data = {
     title: 'Who we are and what you get from our spaces',
     description: `Welcome to LEVEL learning center! We are a cutting-edge learning center founded to provide everyone with high-quality and affordable education, as well as opportunities to study abroad. Our team comprises individuals who have studied abroad and are eager to apply their invaluable experience and methodologies for your benefit. The concept of a hybrid learning center that combines learning with assistance in selecting and applying to universities is not new and is widely practiced globally. However, we are currently the first of its kind in Uzbekistan, with no equivalents. Furthermore, we make education accessible to all and offer unlimited opportunities by providing modern teaching methods and top-notch service. If you are ready to acquire new knowledge and study at prestigious universities worldwide, we are here to help you achieve your goals. `,
     topText: 'About Us',
-    motto: 'Motto',
+    motto: 'Divided by language, united by thoughts',
     why: 'Why you should study at LEVEL learning center',
   },
   header: {
