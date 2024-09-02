6772283bf67bfd52dc31db599cbba652cd29eabd
Tidalu <ul0271069@edu.uni.lodz.pl>
2024-09-02 15:48:31 +0200

    refactor: Remove useState hook and unused event handlers in BenefitCard component

--- components/BenefitCard.tsx
+++ components/BenefitCard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLanguage } from './LanguageContext';
 import Image from 'next/image';
 function BenefitCard({
@@ -10,38 +10,11 @@ function BenefitCard({
   icon: string;
   hoverImage: string;
 }) {
-  const [isHovered, setIsHovered] = useState(false);
   const { language } = useLanguage();
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-  };
 
   return (
-    <div
-      className="relative reveal w-[70%] md:w-[40%] lg:w-[30%] border-gray-800 border-box dark:border-gray-200 border rounded-xl h-auto mt-2 p-4 cursor-pointer"
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-    >
-      <div
-        className={`absolute inset-0 transition-opacity duration-300 ${
-          isHovered ? 'opacity-100' : 'opacity-0'
-        }`}
-      >
-        <img
-          src={hoverImage}
-          alt=""
-          className="w-full h-full object-fill rounded-xl"
-        />
-      </div>
-      <div
-        className={`transition-opacity duration-300 ${
-          isHovered ? 'opacity-0' : 'opacity-100'
-        } bg-transparent`}
-      >
+    <div className="relative reveal w-[100%] md:w-[40%] lg:w-[30%] border-gray-800 border-box dark:border-gray-200 border rounded-xl h-auto mt-2 p-4 cursor-pointer">
+      <div className={` bg-transparent`}>
         <div className="relative flex h-24 justify-center p-4 lg:h-32 w-auto">
           <Image
             src={icon}
@@ -50,6 +23,7 @@ function BenefitCard({
             decoding="async"
             width={100}
             height={100}
+            className=""
             style={{
               width: 'auto',
               height: 'auto',
