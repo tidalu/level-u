0cbdfed5f0e62c30708a0e6a1b8f2bfa72f9dd33
Your Name <you@example.com>
2025-03-25 18:19:30 +0100

    feat: remove underconstuction wrapper

--- app/(pages)/(routes)/merch/page.tsx
+++ app/(pages)/(routes)/merch/page.tsx
@@ -3,11 +3,9 @@ import UnderConstructionWrapper from "@/components/under-construction-wrapper";
 
 function Merch() {
   return (
-    <UnderConstructionWrapper>
       <div className=" relative  mx-auto px-3 pt-36 mb-16  max-w-screen-lg">
         <MerchStore />
       </div>
-    </UnderConstructionWrapper>
   );
 }
 
